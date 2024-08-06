d307ea71ae5d084d9dc42ca3ec1e25d14dbd529f
fennys123 <dsarrazola4@soy.sena.edu.co>
2024-08-06 07:37:23 -0500

    hay comentarios en conection.js y .env para que lo vean

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const URI = `mongodb+srv://${process.env.USERBD}:${process.env.PASSWORDBD}@adso2669736.cdhps8h.mongodb.net/${process.env.BD}?retryWrites=true&w=majority`;
-
+//aca se cambia la uri por la que ustedes tienen en mongo pero se deja las llaves
 mongoose.connect(URI);
 
 module.exports = mongoose;
